03a0246efb402df8cb5e940889e0315c3a7fd1f7
b66690870-sudo <b66690870@gmail.com>
2026-03-03 22:08:01 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from kivy.app import App
+from kivy.uix.button import Button
+class MainApp(App):
+    def build(self):
+        return Button(text='ABU NJM HACKER PRO\nREADY FOR INVEST', background_color=(0,0,0,1), color=(0,1,0,1))
+MainApp().run()
